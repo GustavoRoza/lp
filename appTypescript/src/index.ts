1c455707b204290b9781fee5893778e6093aa3d6
GustavoRoza <gustavofr.contato@gmail.com>
2026-09-07 20:51:56 -0300

    adicionando estudos de typescript

--- appTypescript/src/index.ts
+++ appTypescript/src/index.ts
@@ -1,9 +1,14 @@
+import { randomUUID } from "node:crypto";
+import * as readline from 'readline';
+
+
 interface User {
     id: number;
     name: string;
     email?: string;
     isActive: boolean;
 }
+
 
 function updateUsuario(usuario: User, dados: Partial<Omit<User, 'id'>>): User {
     return {
@@ -26,12 +31,75 @@ const usuario = criarUsuario({
     isActive: true
 });
 
-console.log(usuario);
-
-
-const usuarioAtualizado = updateUsuario(usuario, { name: "Gustavo Fonseca", isActive: false, email: "gustafonseca@gmail.com" });
-console.log(usuarioAtualizado);
 
 
 
+// const usuarioAtualizado = updateUsuario(usuario, { name: "Gustavo Fonseca", isActive: false, email: "gustafonseca@gmail.com" });
+// console.log(usuarioAtualizado);
 
+enum Sexo {
+    macho = "pintudo",
+    femea = "xereca"
+}
+
+enum RacasDeCachorro {
+    ViraLata = "cachorro caramelho",
+    Pinscher = "pinscher",
+    Belga = "belga Malinois"
+}
+
+interface Cachorro {
+    id: string;
+    nome: string;
+    idade: number;
+    raca: RacasDeCachorro;
+    sexo: Sexo;
+}
+
+
+export function cadastrarCachorro(nome: string, idade: number, raca: RacasDeCachorro, sexo: Sexo): Cachorro {
+    const cachorro = {
+        id: randomUUID(),
+        nome,
+        idade,
+        raca,
+        sexo
+    }
+    return cachorro;
+}
+
+
+// const myDog = cadastrarCachorro("lilit", 5, RacasDeCachorro.pinscher, Sexo.femea);
+// console.log(myDog);
+
+
+function recursividade(valor: number): Promise<void> {
+    if (valor === 0) return Promise.resolve();
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(valor);
+            resolve(recursividade(valor - 1));
+        }, 1000);
+    });
+}
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+export async function quiz(): Promise<void> {
+    rl.question("Quanto é 20 + 20? ", async (resposta) => {
+        if (resposta === "40") {
+            await recursividade(3);
+            console.log("Correto!");
+        } else {
+            await recursividade(5);
+            console.log("Errado!");
+        }
+        rl.close();
+    });
+}
+
+// quiz();
